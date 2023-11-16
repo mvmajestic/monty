13844ac56ebcdf6cb90291e54b87dfcb58f43f22
Vuyisanani <vuyisanani@ymail.com>
2023-11-16 11:19:50 -0800

    Added nop opcode

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  *f_nop- will do nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: return nothing
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+
+{
+	(void) counter;
+	(void) head;
+}
