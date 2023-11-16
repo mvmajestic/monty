e16e2bbd11862b1705c196e740f365c7b9692028
Vuyisanani <vuyisanani@ymail.com>
2023-11-16 10:27:50 -0800

    Added function files

---
+++ pr_str.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+/**
+ * f_pstr - will print string starting at top of stack, followed
+ * by a new line
+ * @head: stack head
+ * @counter: line_number
+ * Return: return nothing
+*/
+
+void f_pstr(stack_t **head, unsigned int counter)
+
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	while (h)
+	{
+		if (h->n > 127 || h->n <= 0)
+		{
+			break;
+		}
+		printf("%c", h->n);
+		h = h->next;
+	}
+	printf("\n");
+}
